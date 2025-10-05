c5867f4ad9732c283a9cd255250511bdd67e6797
TintingPatch <83007965+TintingPatch@users.noreply.github.com>
2025-10-05 15:22:49 +0200

    Update to version 1.1 and improve plugin instancing
    Bumped version to 1.1 in pom.xml and plugin.yml. Enhanced ModMode.java to display a startup banner, add debug logging, and prevent multiple plugin instances by checking for existing versions and disabling older or duplicate instances.

--- src/main/java/com/tintingpatch/modMode/ModMode.java
+++ src/main/java/com/tintingpatch/modMode/ModMode.java
@@ -6,11 +6,13 @@ import com.tintingpatch.modMode.listeners.JoinQuitListeners;
 import com.tintingpatch.modMode.managers.BStatsManager;
 import com.tintingpatch.modMode.utils.CustomConfig;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ModMode extends JavaPlugin {
     //TODO: Time Checkup
-
+    boolean production = true;
 
     static CustomConfig customConfig;
     static ModMode instance;
@@ -18,12 +20,39 @@ public final class ModMode extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        if(instance == null){
-            instance = this;
-        }else {
-            getComponentLogger().error("Second instance of main file has been created. Preventing...");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "███    ███  ██████  ██████  ███    ███  ██████  ██████  ███████");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "████  ████ ██    ██ ██   ██ ████  ████ ██    ██ ██   ██ ██");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "██ ████ ██ ██    ██ ██   ██ ██ ████ ██ ██    ██ ██   ██ █████");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "██  ██  ██ ██    ██ ██   ██ ██  ██  ██ ██    ██ ██   ██ ██");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GOLD + "██      ██  ██████  ██████  ██      ██  ██████  ██████  ███████" + ChatColor.AQUA + " v." + getPluginMeta().getVersion());
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "┏┓ ╻ ╻      ╺┳╸╻┏┓╻╺┳╸╻┏┓╻┏━╸┏━┓┏━┓╺┳╸┏━╸╻ ╻");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "┣┻┓┗┳┛       ┃ ┃┃┗┫ ┃ ┃┃┗┫┃╺┓┣━┛┣━┫ ┃ ┃  ┣━┫");
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GRAY + "┗━┛ ╹        ╹ ╹╹ ╹ ╹ ╹╹ ╹┗━┛╹  ╹ ╹ ╹ ┗━╸╹ ╹");
+        debugLog("This is a not production version of this plugin.");
+
+        if(this.getConfig().getBoolean("inDebug")){
+            production = false;
+            debugLog("Activated debug mode in production version.");
+        }
+        //prevent double instancing
+        for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+            if(plugin.getName().equals(this.getPluginMeta().getName()) && plugin != this){
+                if(Integer.parseInt(plugin.getPluginMeta().getVersion().split("\\.")[0]) >= Integer.parseInt(this.getPluginMeta().getVersion().split("\\.")[0]) || Integer.parseInt(plugin.getPluginMeta().getVersion().split("\\.")[1]) >= Integer.parseInt(this.getPluginMeta().getVersion().split("\\.")[1])){
+                    this.getLogger().info("Found other newer or equal instance of this plugin. Disabling this plugin...");
+                    Bukkit.getPluginManager().disablePlugin(this);
+                    return;
+                }else {
+                    this.getLogger().info("Found other older instance of this plugin. Disabling older plugin...");
+                    Bukkit.getPluginManager().disablePlugin(plugin);
+                    return;
+                }
+            }
+        }
+        if(instance != null){
+            this.getLogger().info("Found another instance of main class. Disabling...");
             return;
         }
+        instance = this;
         getComponentLogger().info("ModMode by TintingPatch enabled");
         saveDefaultConfig();
         customConfig = new CustomConfig("data.yml", getDataFolder());
@@ -35,6 +64,12 @@ public final class ModMode extends JavaPlugin {
         Bukkit.getPluginCommand("modmode").setExecutor(new ModModeCommand());
         Bukkit.getPluginManager().registerEvents(new AttackListener(), this);
         Bukkit.getPluginManager().registerEvents(new JoinQuitListeners(), this);
+    }
+
+    public void debugLog(String msg){
+        if(!this.production){
+            this.getLogger().info("Debug: " + msg);
+        }
     }
 
     @Override
